a946e23a5a60a23fa53a7bc531836b15f0d2353a
sumit2010-apk <132653810+sumit2010-apk@users.noreply.github.com>
2026-05-06 21:39:43 +0530

    Update load_data.py

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 def load_data(path):
     df = pd.read_excel(path)
 
-    # Rename columns properly
+    
     df.columns = ['state', 'date', 'total', 'category']
 
     return df
